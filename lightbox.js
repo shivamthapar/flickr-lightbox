1df30488edcac861f15cdd6d516836728ae715e7
Shivam Thapar <shivam.thapar1@gmail.com>
2015-12-24 12:38:33 -0800

    Add basic Lightbox component, rename to Gallery
    * Rename outermost module to Gallery, with a lightbox
      component (when the fullsize photo is viewed)
    * Add basic Lightbox module
    * When thumbnail clicked, Lightbox shows fullsize pic
    * Cannot close/hide lightbox yet
    * Add Lightbox centering logic

--- lightbox.js
+++ lightbox.js
@@ -47,22 +47,89 @@ Flickr.prototype = (function(){
   };
 }());
 
-var Lightbox = (function(window) {
+var Lightbox = function(photos, parentElem){
+  // private members
+  this.photos = photos;
+  this.parentElem = parentElem;
+  this.visible = false;
+  this.currIndex = 0;
 
-  //constructor
-  function Lightbox(divId, flickrApiKey) {
+  this.displayCurrPhoto = function(){
+    var photo;
+    if(!this.elem.firstChild){
+      photo = document.createElement("img");
+      photo.setAttribute("id", "lightbox-photo");
+      this.elem.appendChild(photo);
+    }
+    else{
+      photo = this.elem.firstChild;
+    }
+    photo.setAttribute("src", this.photos[this.currIndex].url);
+    this.setVisible(true);
+    photo.onload = this.onPhotoLoadHandler;
+    console.log(photo);
+  }
+
+  this.setVisible = function(b){
+    this.visible = b;
+    if(!this.visible){
+      if(this.elem.getAttribute("class") !== "hide")
+        this.elem.setAttribute("class", "hide");
+    }
+    else{
+      this.elem.setAttribute("class","");
+    }
+  }
+
+  this.createLightboxElem = function(){
+    this.elem = document.createElement("div");
+    this.elem.setAttribute("id", "lightbox");
+    this.setVisible(this.visible);
+    parentElem.appendChild(this.elem);
+  }
+
+  this.onPhotoLoadHandler = function(){
+    // center picture
+    this.style.width = this.width;
+    this.style.height = this.height;
+    this.style.marginTop = "-" + this.height/2 + "px";
+    this.style.marginLeft = "-" + this.width/2 + "px";
+  }
+
+  this.createLightboxElem();
+}
+Lightbox.prototype = (function(){
+  return {
+    setCurrIndex: function(idx){
+      this.currIndex = idx;
+      this.displayCurrPhoto();
+    },
+    setVisible: this.setVisible
+  };
+}());
+
+var Gallery = (function(window) {
+
+  function Gallery(divId, flickrApiKey) {
     var self = this;
+
+    var thumbClickHandler = function(){
+      self.lightbox.setCurrIndex(parseInt(this.getAttribute("id")));
+    };
+
     this.div = document.getElementById(divId);
     this.flickrUser = this.div.getAttribute('data-flickr-user');
     this.flickrPhotoset = this.div.getAttribute('data-flickr-photoset');
     this.photos = [];
     this.flickr = new Flickr(flickrApiKey);
+    this.lightbox = null;
 
     this.fetchImages = function(user,photoset){
       this.flickr.getImages(user, photoset, function(success, data){
         if(success){
           self.photos = data.photos;
           self.createGrid();
+          self.lightbox = new Lightbox(self.photos, self.div);
         }
         else{
           console.log("error: ", error);
@@ -73,23 +140,24 @@ var Lightbox = (function(window) {
     this.createGrid = function(){
       var grid = document.createElement("div");
       grid.setAttribute("id", "lightbox-grid");
-      this.photos.forEach(function(photo){
+      this.photos.forEach(function(photo, i){
         var p = document.createElement("img");
         p.setAttribute("class", "lightbox-thumb");
+        p.setAttribute("id", i);
         p.setAttribute("src", photo.thumb);
         p.setAttribute("data-large", photo.url);
+        
+        p.addEventListener("click", thumbClickHandler, false);
         grid.appendChild(p);
       });
       this.div.appendChild(grid);
 
     };
 
-    this.addImage = function(){
-    };
     this.fetchImages(this.flickrUser,this.flickrPhotoset);
 
   }
 
-  return Lightbox;
+  return Gallery;
   
 })(window);
